cd71a4cb262aaa960bcbf503039a83e15e8b9e13
Tony Pitale <tpitale@gmail.com>
2012-10-08 23:19:47 -0400

    updated example of extract including retailer and package

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -2,8 +2,11 @@ class ImportsController < ApplicationController
   before_filter :require_token
 
   def create
+    retailer = params[:retailer]
+    retailer_ics_id = retailer["ezport"].to_i - 30_000 # => 999 for our fictional retailer
+
     # optionally, in Thread.new { ... }
-    params[:import].each do |model_name, collection|
+    params[:package].each do |model_name, collection|
       klass = model_name.camelize.constantize # will find Item, etc.
 
       collection.each do |attributes|
@@ -25,7 +28,7 @@ class ImportsController < ApplicationController
   #   request.url 'http://localhost:3111/import'
   #   request.options[:timeout] = 5
   #   request.headers['Content-Type'] = 'application/json'
-  #   request.body = {"item" => [1,2,3]}.to_json
+  #   request.body = {"retailer" => {"id" => 12, "name" => "Beer Haus", "ezport" => 30999}, "package" => {"item" => [1,2,3]}}.to_json
   # end
 
   private
